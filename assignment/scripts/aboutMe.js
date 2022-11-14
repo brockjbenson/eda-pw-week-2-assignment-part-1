f0bbe1d2971a54b1887c9a3bb6a6166c199c3c30
Brock Benson <brockjbenson@outlook.com>
2022-11-14 16:42:21 -0600

    first and last

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,11 +1,11 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-
+let firstName='Brock';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName='Benson';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-
+let fullName=''
 // 4 - Console log the value of `fullName`
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
